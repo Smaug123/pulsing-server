deff47cc8f5d80ec1c766543a2270c26a0c21060
Smaug123 <patrick+github@patrickstevens.co.uk>
2021-02-17 18:39:57 +0000

    Add test script

---
+++ test.fsx
@@ -0,0 +1,19 @@
+open System.Net
+open System.Diagnostics
+
+use timer = new Stopwatch()
+timer.Restart()
+
+[
+    for i in 1..1000 do
+        yield
+            async {
+                let w = new WebClient()
+                return w.DownloadString("http://localhost:5000/")
+            }
+]
+|> Async.Parallel
+|> Async.RunSynchronously
+
+timer.Stop()
+printfn "%+A" timer.ElapsedMilliseconds
